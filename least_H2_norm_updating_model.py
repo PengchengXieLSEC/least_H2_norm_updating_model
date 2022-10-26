f43b10a7e103cd3c6136fd665d5258dc3a7b6c30
PengchengXieLSEC <xpc@lsec.cc.ac.cn>
2022-10-27 01:03:37 +0800

    Update least_H2_norm_updating_model.py

--- least_H2_norm_updating_model.py
+++ least_H2_norm_updating_model.py
@@ -30,23 +30,12 @@ def quad_Frob(X, F_values, args):
     """
     Given a set of points in the trust region
     and their values, construct a quadratic model
-    in the form of g.T x + 1/2 x.T H x + \alpha.
-
-    If the number of points are less than
-    (n+1) (n+2)/2 then build the model such that the
-    Frobenius norm is minimized. In this code the KKT
-    conditions are solved. Otherwise, solve
-    the system of equations used in polynomial interpolation.
-    M(\phi, Y) \lambda = f
-
-    arguments: X: the sample points to be interpolated
-        F_values: the corresponding true solutions to the sample points
-    outputs: g and H in the quadratic model
+    
     """
-    # Minimum value accepted for a singular value
+    
     eps = np.finfo(float).eps
     tol_svd = eps ** 5
-    # n = number of variables m = number of points
+    
     (n, m) = X.shape
 
     H = np.zeros((n, n))
@@ -79,15 +68,13 @@ def quad_Frob(X, F_values, args):
                 A[i, j] = A[i, j] - (omega3 / (8 * omega1 * (n * omega3 + omega1))) * np.dot(Y[:, i].T,Y[:, i]) * np.dot(Y[:, j].T, Y[:, j])
 
         J = np.zeros((m, 1))
-        # X_bar = np.zeros((1, m))
-        # B1 = np.zeros((1, m))
+        
         for i in range(m):
             J[i] = 1 - omega4 / (4 * omega1 + 4 * n * omega3) * np.dot(Y[:, i].T, Y[:, i])
         
 
         line1 = np.hstack((A, J, Y.T))  
-        line2 = np.hstack((J.T, ((n * omega4 ** 2) / (2 * n * omega3 + 2 * omega1) - 2 * omega5) * np.ones((1, 1)),
-                           np.zeros((n, 1)).T))
+        line2 = np.hstack((J.T, ((n * omega4 ** 2) / (2 * n * omega3 + 2 * omega1) - 2 * omega5) * np.ones((1, 1)), np.zeros((n, 1)).T))
         line3 = np.hstack((Y, np.zeros((n, 1)), -2 * omega2 * np.identity(n)))
 
         W = np.vstack((line1, line2, line3))
@@ -95,10 +82,10 @@ def quad_Frob(X, F_values, args):
         lambda_0 = _compute_coeffs(W, tol_svd, b, option='partial')  
 
         # Grab the coeffs of linear terms (g) and the ones of quadratic terms
-        # (H) for g.T s + s.T H s
+        
         c = lambda_0[m:m + 1]
         g = lambda_0[m + 1:m + 1 + n]
-        # T = lambda_0[m + 1 + n]
+        
 
         H = np.zeros((n, n))  
         
@@ -106,20 +93,18 @@ def quad_Frob(X, F_values, args):
         for j in range(m):
             inner_sum += lambda_0[j] * np.dot(Y[:, j].reshape(1, n), Y[:, j].reshape(n, 1))
 
-        H = H - (1 / (2 * omega1)) * (2 * omega3 * (
-                    (1 / (2 * (2 * n * omega3 + 2 * omega1))) * inner_sum - n * omega4 * c / (
-                        2 * n * omega3 + 2 * omega1)) + omega4 * c) * np.identity(n)
+        H = H - (1 / (2 * omega1)) * (2 * omega3 * ((1 / (2 * (2 * n * omega3 + 2 * omega1))) * inner_sum - n * omega4 * c / (2 * n * omega3 + 2 * omega1)) + omega4 * c) * np.identity(n)
         for j in range(m):
             H = H + 1 / (4 * omega1) * (lambda_0[j] * np.dot(Y[:, j].reshape(n, 1), Y[:, j].reshape(1, n)))
 
 
     else:  # Construct a full model
-        # Here we have enough points. Solve the sys of equations.
+        # Here we have enough points. 
         b = F_values
         phi_Q = np.array([])
         for i in range(m):
             y = Y[:, i]
-            y = y[np.newaxis]  # turn y from 1D to a 2D array
+            y = y[np.newaxis]  
             aux_H = y * y.T - 0.5 * np.diag(pow(y, 2)[0])
             aux = np.array([])
             for j in range(n):
@@ -132,7 +117,7 @@ def quad_Frob(X, F_values, args):
 
         lambda_0 = _compute_coeffs(W, tol_svd, b, option='full')
 
-        # Retrieve the model coeffs (g) and (H)
+        # Retrieve (g) and (H)
         g = lambda_0[1:n + 1, :]
         cont = n + 1
         H = np.zeros((n, n))
