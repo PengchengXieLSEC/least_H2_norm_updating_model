69f70a68d8c57ebe5c303d46832b4bf667a24654
Pengcheng Xie <xpc@lsec.cc.ac.cn>
2023-04-13 01:49:23 +0800

    Update dfo_tr.m

--- DFO-H2-norm-matlab-version-1.0/dfo_tr.m
+++ DFO-H2-norm-matlab-version-1.0/dfo_tr.m
@@ -3,7 +3,7 @@
 %Email: xpc@lsec.cc.ac.cn
 
 function [res, iteration, f_hist] = dfo_tr(bb_func, x_initial, clist, options)
-  %UNTITLED7 此处显示有关此函数的摘要
+ 
   %   此处显示详细说明
   f_hist = [];
 
@@ -11,7 +11,7 @@ function [res, iteration, f_hist] = dfo_tr(bb_func, x_initial, clist, options)
   t1 = clock;
 
   %%
-  % see the param class for the description of parameters
+  
   all_options = struct("delta", 1.0, ... % initial delta (i.e. trust region radius)
   "tol_delta", 1e-10, ... % smallest delta to stop
     "max_delta", 100.0, ... % max possible delta
@@ -51,7 +51,7 @@ function [res, iteration, f_hist] = dfo_tr(bb_func, x_initial, clist, options)
   min_del_crit = all_options.min_del_crit;
   min_s_crit = all_options.min_s_crit;
 
-  %%
+  
   % set the verbosity parameter
   if ismember("verbosity", fieldnames(options))
     verb = options.verbosity;
@@ -79,8 +79,7 @@ function [res, iteration, f_hist] = dfo_tr(bb_func, x_initial, clist, options)
 
   %%
   % func: _build_initial_sample,
-  % input:x_initial, delta, options,
-  % output:Y, nY
+
   func_n = size(x_initial, 1);
   if ~isempty(options) || ~ismember("sample_gen", fieldnames(options))
     option_build = "auto";
@@ -132,22 +131,17 @@ function [res, iteration, f_hist] = dfo_tr(bb_func, x_initial, clist, options)
   while 1
     success = 0;
 
-    % shift_Y = Y - np.dot(np.diag(Y[:, 0]), np.ones((n, nY)))
-    % print('被用来减去的Q_value的Y', Y)
     for j = 1:nY
       Yzhuan = reshape(Y(1:end, j), 1, n);
       g_hat_hist_rot = reshape(g_hat_hist(1:end, 1, end), 1, n);
       Q_value(j) = 0.5 * Yzhuan * H_hist(1:end, 1:end, end) * Y(1:end, j) + g_hat_hist_rot * Y(1:end, j) + c_hat_hist(end);
     end
-    % print('被用来减去的Q_value', Q_value)
-    % print('被用来减去的f_value', f_values)
+  
 
     f_min_Q_values = f_values - Q_value;
-    % print('f_min_Q', f_min_Q_values)
-    % construct the quadratic model
+  
     [H, g_hat, c_hat] = quad_frob(Y, f_min_Q_values, clist);
-    % 加前一步的H和g，没有c; 第k轮，加上第k-1的H和g，c  *** 2
-    % print('Hg',(H, g))
+
     Q_test = zeros(nY, 1);
     for j = 1:nY
       Q_test(j) = 0.5 * Y(1:end, j)' * H * Y(1:end, j) + g_hat' * Y(1:end, j) + c_hat;
